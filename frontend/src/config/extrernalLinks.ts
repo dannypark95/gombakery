e1fe2b5108c35773b0e251049ca61be3fe52bf60
“Daniel <dannypark95@gmail.com>
2024-01-15 15:50:20 -0600

    new change

--- frontend/src/config/extrernalLinks.ts
+++ frontend/src/config/extrernalLinks.ts
@@ -1,6 +1,7 @@
 const externalLinks = {
   instagram: 'https://www.instagram.com/gom.bakery/',
   order: 'https://neartail.com/sm/oUuIOzE0t',
+  cdn: 'https://d3hu2u8k16gig2.cloudfront.net/',
 };
 
 export default externalLinks;
--- frontend/src/config/videos.ts
+++ frontend/src/config/videos.ts
@@ -1,11 +1,16 @@
-import home_video_1 from '../assets/videos/baking_1.mp4';
+// import home_video_1 from '../assets/videos/baking_1.mp4';
 import home_video_2 from '../assets/videos/baking_2.mp4';
 import home_video_3 from '../assets/videos/baking_3.mp4';
+import externalLinks from './extrernalLinks';
 
 const videos = {
-  home_video_1,
-  home_video_2,
-  home_video_3,
+  home_video_1: `${externalLinks.cdn}/baking_1.mp4`,
 };
+
+// const videos = {
+//   home_video_1,
+//   home_video_2,
+//   home_video_3,
+// };
 
 export default videos;
