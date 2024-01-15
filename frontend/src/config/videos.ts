a57914ce953c953ae54565f684006e6530355ce5
“Daniel <dannypark95@gmail.com>
2024-01-15 16:04:19 -0600

    feature: cdn test

--- frontend/src/config/videos.ts
+++ frontend/src/config/videos.ts
@@ -1,7 +1,13 @@
+import externalLinks from './extrernalLinks';
+
 const videos = {
-  home_video_1: 'https://d3hu2u8k16gig2.cloudfront.net/video/baking_1.mp4',
-  home_video_2: 'https://d3hu2u8k16gig2.cloudfront.net/video/baking_2.mp4',
-  home_video_3: 'https://d3hu2u8k16gig2.cloudfront.net/video/baking_3.mp4',
+  home_video_1: `${externalLinks.cloudFrontBaseUrl}/baking_1.mp4`,
 };
+
+// const videos = {
+//   home_video_1: 'https://d3hu2u8k16gig2.cloudfront.net/video/baking_1.mp4',
+//   home_video_2: 'https://d3hu2u8k16gig2.cloudfront.net/video/baking_2.mp4',
+//   home_video_3: 'https://d3hu2u8k16gig2.cloudfront.net/video/baking_3.mp4',
+// };
 
 export default videos;
